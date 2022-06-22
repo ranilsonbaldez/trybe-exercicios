8c81ad9bf1bc604fd11931379f4d9c13a7e0fc51
Ranilson Baldez <ranilson12@gmail.com>
2022-06-21 22:58:50 -0300

    Programa retorna o maior de tres numeros ok

--- 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-e-primeiros-passos/3-maior-de-tres-numeros.js
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-e-primeiros-passos/3-maior-de-tres-numeros.js
@@ -3,11 +3,11 @@ const num2 = 14;
 const num3 = 2;
 
 if (num1 > num2 && num1 > num3) {
-    console.log(num1);
+    console.log(num1 + " é o maior numero.");
 }
 else if (num2 > num1 && num2 > num3) {
-    console.log(num2);
+    console.log(num2 + " é o maior numero.");
 }
 else {
-    console.log(num3);
+    console.log(num3 + " é o maior numero.");
 }
