c285f73d206b0093f02210d44ee2871056a74221
Ranilson Baldez <ranilson12@gmail.com>
2022-06-22 18:52:54 -0300

    exibe a média aritmética dos valores da array

---
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicio3.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;       //declara a variável soma com o valor inicial zero
+
+for (index = 0; index < numbers.length; index += 1){    //cria o laço do contador
+    soma += numbers[index];     //faz o incremanto de cada valor e coloca na variável soma
+}
+
+console.log(soma / 10);      //exibe o resultado da M.A dos 10 valores
