5ef73aabacca5cbdda4a98d413c03abf169b5da0
Ranilson Baldez <ranilson12@gmail.com>
2022-06-21 23:58:25 -0300

    Se pelo menos um numero for par

---
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-e-primeiros-passos/8-pelo-menos-um-par.js
@@ -0,0 +1,14 @@
+const num1 = 65;
+const num2 = 15;
+const num3 = 3;
+
+let resultado;
+
+if ((num1 % 2 === 0 || num2 % 2 === 0 || num3 & 2 ===0)) {
+    resultado = true;
+    console.log(resultado);
+}
+else {
+    resultado = false;
+    console.log(resultado);
+}
