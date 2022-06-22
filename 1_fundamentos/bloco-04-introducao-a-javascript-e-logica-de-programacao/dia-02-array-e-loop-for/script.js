1bc5763c5584099b53757565e2a7151e22509570
Ranilson Baldez <ranilson12@gmail.com>
2022-06-22 18:10:03 -0300

    soma dos valores contidos na array numbers

--- 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script.js
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/script.js
@@ -1,3 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (index = 0; index < numbers.length; index += 1)
-console.log(numbers[index]);
+let soma = 0;       //declara a variável soma com o valor inicial zero
+
+for (index = 0; index < numbers.length; index += 1){    //cria o laço do contador
+    soma += numbers[index];     //faz o incremanto de cada valor e coloca na variável soma
+}
+
+console.log(soma);      //exibe o resultado de soma
+
+
+
+
