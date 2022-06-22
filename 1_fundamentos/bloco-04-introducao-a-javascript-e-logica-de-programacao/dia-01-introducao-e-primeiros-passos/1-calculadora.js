0475ca3bb07233014b935f9c5a761db70ba369f3
Ranilson Baldez <ranilson12@gmail.com>
2022-06-21 22:19:48 -0300

    Programa com operações aritméticas

---
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-e-primeiros-passos/1-calculadora.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 5;
+
+let soma = (a + b);
+let subtracao = (a - b);
+let multiplicacao = (a * b);
+let divisao = ( a / b);
+let modulo = (a % b);
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
