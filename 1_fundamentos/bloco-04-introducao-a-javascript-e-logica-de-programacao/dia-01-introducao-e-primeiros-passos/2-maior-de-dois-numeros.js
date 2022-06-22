2e9a2614fadea2a058e3dbeb95f57fb1c6e39799
Ranilson Baldez <ranilson12@gmail.com>
2022-06-21 22:56:06 -0300

    Programa retorna o maior de dois numeros ok

--- 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-e-primeiros-passos/2-maior-de-dois-numeros.js
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-introducao-e-primeiros-passos/2-maior-de-dois-numeros.js
@@ -2,8 +2,8 @@ const num1 = 8;
 const num2 = 13;
 
 if (num1 > num2) {
-    console.log(num1);
+    console.log(num1 + " é maior do que " + num2);
 }
 else {
-    console.log(num2);
+    console.log(num2 + " é maior do que " + num1);
 }
