8675cd36c5efa9052d09c85210c0d36a9ce2e7eb
Ranilson Baldez <ranilson12@gmail.com>
2022-06-23 16:14:51 -0300

    verifica se o valor da média é  maior ou menor igual a 20

--- 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicio4.js
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-e-loop-for/exercicio4.js
@@ -1,9 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-let soma = 0;       //declara a variável soma com o valor inicial zero
+let soma = 0; //declara a variável soma com o valor inicial zero
 
-for (index = 0; index < numbers.length; index += 1){    //cria o laço do contador
-    soma += numbers[index];     //faz o incremanto de cada valor e coloca na variável soma
+for (index = 0; index < numbers.length; index += 1) { //cria o laço do contador
+    soma += numbers[index]; //faz o incremanto de cada valor e coloca na variável soma
 }
 
 let media = (soma / 10);
@@ -12,7 +12,6 @@ console.log(media);
 
 if (media > 20) {
     console.log('Valor maior que 20.');
-}
-else {
+} else {
     console.log('Valor menor ou igual a 20.');
 }
