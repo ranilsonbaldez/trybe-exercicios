5103c5b18b411ff157bb007a49ab9a41c4837468
Ranilson Baldez <ranilson12@gmail.com>
2022-06-26 18:36:43 -0300

    iprime a quantidade de arrays dentro do objeto

---
+++ 1_fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-objetos-e-funcoes/exercicio8.js
@@ -0,0 +1,26 @@
+//Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [{
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+    }, ],
+};
+
+leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+})
+
+quantidadeLivros = 0;
+
+for (let x of leitor.livrosFavoritos) {
+    quantidadeLivros += 1;
+}
+
+console.log(leitor.nome + ' tem ' + quantidadeLivros + ' livros favoritos.');
